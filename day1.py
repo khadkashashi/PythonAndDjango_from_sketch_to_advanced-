db213314836fabdf17ef2376d8862c664a86b18d
shashi khadka <shashikhadka84@gmail.com>
2026-04-21 18:20:36 +0545

    Initial commit

---
+++ day1.py
@@ -0,0 +1,18 @@
+print("hello world","testing","hello")
+
+a = 10
+test = 1
+TEST = 1
+tesT ="shashi"#cammal case
+test = 100
+test_test =1 #best,also snake case, maxiuse
+_test =1
+test_ =4
+#test-test-test =1 #kabab case
+test2="hello"
+
+#3test=1
+#test test
+# print =1
+#@test = 2
+print(test)
