79a223f34a05b36ede4ec8595e98aafd4f6b5f2a
shashi khadka <shashikhadka84@gmail.com>
2026-04-23 18:02:47 +0545

    opepation--> arithmatic , logical

---
+++ day3.py
@@ -0,0 +1,47 @@
+print(1+1)
+print(5-2)
+print(7-6)
+print(10/7)
+
+
+#** __> exponential, only works in integer
+print(2**10)
+#% modulas
+print(10%3)
+#// double slace, only answer in int.use when point number is not imp
+print (10//3)
+
+
+# for string +,*  +--> concatination
+a= "hello"
+b="world"
+print (a+b)
+
+a= "hello"
+b= 55
+print (a+str(b))#type casting
+
+
+#pydantic v2-->  manage conflict of data manage.
+
+
+#multiplication for string, one variable should be int.
+a="hello"
+b=3
+print(a*b)
+
+#comparision operator --> ==,<>,<=,>=,!=, answer is either true or false. used loops, comparisons, filtering, if/else statement, loops and conditions.
+#= --> data assign, == -->value compare
+
+
+print ("sudan"!="sudan")
+print (5>10)
+print (5<10)
+print (5!=3)
+print(4<=6)
+print(5==5)
+print (6>=6)
+
+#and or not
+print(True and False)
+print ("sudan" == "sudan" and 1<=100 )
