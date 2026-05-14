f7bc1dff57579a6371c7602856849a27e70e8c6b
shashi khadka <shashikhadka84@gmail.com>
2026-05-14 23:35:57 +0545

    git and github

--- day11.py
+++ day11.py
@@ -2,4 +2,10 @@
 #private key----->decrption
 
 #git----> Git is version control system. Git helps to keep track of code changes . It is used to collaborate on code
+#.gitignore
+# git init .
+#git add .
+#git commit - m " "
+#git remote add origin (variable) repo name
 
+#git status--->
